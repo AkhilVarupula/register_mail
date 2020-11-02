54a9004de617d0e5ba60f910dd6cf9ae51f564b3
Akhil <akhilbpm@gmail.com>
2020-11-02 11:15:20 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+var faker= require('faker');
+var mysql= require('mysql');
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database:"node_project",
+  password:'mymacmysql',
+  insecureAuth : true
+});
+q='SELECT CURDATE() AS date';
+connection.query(q, function(error, results, fields) {
+    if (error) throw error;
+    console.log(results[0].date);
+  });
+  connection.end();
